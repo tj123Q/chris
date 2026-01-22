703a8927291ffd48e4708ed0cc2e3bd3dcc15a3a
tj123Q <gordonchristian881@gmail.com>
2026-01-22 22:55:10 +0000

    rps

--- rps.py
+++ rps.py
@@ -1,7 +1,7 @@
 from random import randint
 player_wins = 0
 cpmputer_wins = 0 
-tie_counter = 0 
+tie_player = 0 
 while True:
     print("do you waht to paly a game for fun? ")
     print("1.cotinue to game")
@@ -12,11 +12,27 @@ while True:
     if choice == 1:
         computer =  randint(0,2)
         print(" waht number do you chose? ")
-        user = int(input("choice 0 == rock , 1 == paper , 2 == sissors"))
-        if computer == user:
+        player = int(input("choice 0 == rock , 1 == paper , 2 == sissors"))
+        if computer == player:
+            print(f 'computer number = {computer}')
+            print(f 'player number = {player}')
             print("its a tie!")
-            tie_counter += 1
+            tie_player += 1
+         
+        
+    
+        if computer == player:
+            print("player wins🎉")
+            player_wins += 1
+            computer_lost += 1
+       
+        if computer == player:
+            print(f 'computer number = {computer}')
+            print(f 'player number = {player}')
+            print("you lost💀")    
+            player_lost += 1
+            computer_wins += 1
     if choice == 2:
     
-    if choice == 3:
-        break
+        if choice == 3:
+            break
